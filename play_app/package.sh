6a50ade16fce0b45d8b40b7ecb14c35febc1373b
bin yu <yubin154@gmail.com>
2014-09-23 15:17:23 +0000

    play app

--- play_app/package.sh
+++ play_app/package.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
+# DIR: root package directory, default current working dir
+# appname: name of the app
+# version: version of the app
+# pkgsrc: what to include in the package, default all non hidden files and dirs
+# platform: optional platform, script will generate default value if not provided.
+# 
+# Package $appname-$version.$platform.cronus is in folder target_cronus
 
 rm -rf target_cronus
 
-DIR=$(cd "$(dirname "$0")"; pwd)
-appname="myapp"
-version="0.1.`date +%Y%m%d%H%M`"
-pkgsrc=`ls $DIR`
+DIR=$(cd "$(dirname "$0")"; pwd) # default current dir
+appname="play_hw" # name of the app
+version="0.1.`date +%Y%m%d%H%M`" # package version
+pkgsrc=`ls $DIR` # include everything in package
 
-curl -sSL 'http://www.stackscaling.com/downloads/package_cronus' | DIR=$DIR appname=$appname version=$version pkgsrc=$pkgsrc bash
+wget -qO- 'http://www.stackscaling.com/downloads/package_cronus' | DIR=$DIR appname=$appname version=$version pkgsrc=$pkgsrc bash
 
 mkdir target_cronus
 mv *.cronus *.cronus.prop target_cronus/
