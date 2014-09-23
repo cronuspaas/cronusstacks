b79e0ecdf3aaa4fec2a641f1fef29990d0e69b99
bin yu <yubin154@gmail.com>
2014-09-23 07:24:53 +0000

    standardize

--- simple_pyapp/package.sh
+++ simple_pyapp/package.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+# DIR: root package directory, default current working dir
+# appname: name of the app
+# version: version of the app
+# pkgsrc: what to include in the package, default all non hidden files and dirs
+# platform: optional platform, script will generate default value if not provided.
+# 
+# Package $appname-$version.$platform.cronus is in folder target_cronus
 
 rm -rf target_cronus
 
-DIR=$(cd "$(dirname "$0")"; pwd)
-appname="simple_pyserver"
-version="1.0.0"
-pkgsrc=`ls $DIR`
+DIR=$(cd "$(dirname "$0")"; pwd) # default current dir
+appname="pyserver" # name of the app
+version="1.0.0" # package version
+pkgsrc=`ls $DIR` # include everything in package
+platform="all"
 
-curl -sSL 'http://www.stackscaling.com/downloads/package_cronus' | DIR=$DIR appname=$appname version=$version pkgsrc=$pkgsrc platform=all bash
+wget -qO- 'http://www.stackscaling.com/downloads/package_cronus' | DIR=$DIR appname=$appname version=$version pkgsrc=$pkgsrc platform=$platform bash
 
 mkdir target_cronus
 mv *.cronus *.cronus.prop target_cronus/
